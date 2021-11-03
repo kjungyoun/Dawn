43dc22ed6a5f7d388c3d3ea90f873e5cf13ecc99
Huijiny <jiin20803@gmail.com>
2021-11-03 18:01:46 +0900

    팩맨 조이스틱 구현

---
+++ Assets/Script/VirtualJoystick.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class VirtualJoystick : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
+{
+    private Image imageBackground;
+    private Image imageController;
+    private Vector2 touchPosition;
+
+    private void Awake()
+    {
+        imageBackground = GetComponent<Image>();
+        imageController = transform.GetChild(0).GetComponent<Image>();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        touchPosition = Vector2.zero;
+
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            imageBackground.rectTransform, eventData.position, eventData.pressEventCamera, out touchPosition))
+        {
+            touchPosition.x = (touchPosition.x / imageBackground.rectTransform.sizeDelta.x);
+            touchPosition.y = (touchPosition.y / imageBackground.rectTransform.sizeDelta.y);
+
+            touchPosition = new Vector2(touchPosition.x * 2 - 1, touchPosition.y * 2 - 1);
+
+            touchPosition = (touchPosition.magnitude > 1) ? touchPosition.normalized : touchPosition;
+
+            imageController.rectTransform.anchoredPosition = new Vector2(
+                touchPosition.x * imageBackground.rectTransform.sizeDelta.x / 4,
+                touchPosition.y * imageBackground.rectTransform.sizeDelta.y / 4);
+
+            Debug.Log("Touch Position :" + touchPosition.x );
+            Debug.Log("Touch Position :" + touchPosition.y);
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        imageController.rectTransform.anchoredPosition = Vector2.zero;
+        touchPosition = Vector2.zero;
+    }
+
+    public float Horizontal()
+    {
+        return touchPosition.x;
+    }
+
+    public float Vertical()
+    {
+        return touchPosition.y;
+    }
+}
