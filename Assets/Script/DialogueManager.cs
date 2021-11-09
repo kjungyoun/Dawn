84d8957c8a5a869e9da5c497743ca872ce90b88f
kjungyoun <rlawjddbs963@naver.com>
2021-11-10 04:40:03 +0900

    [feat] Name Input Feature Update

--- Assets/Script/DialogueManager.cs
+++ Assets/Script/DialogueManager.cs
@@ -135,7 +135,26 @@ public class DialogueManager : MonoBehaviour
         // 텍스트 출력
         for(int i = 0; i<listSentences[count].Length; i++)
         {
-            text.text += listSentences[count][i]; // 1번째 문장, 가나다라마바사 한 글자씩 출력
+            if(listSentences[count][i].Equals('뜐')) // 주인공 이름일 때
+            {
+                for(int j=0; j<Dialogue.yourName.Length; j++)
+                {
+                    text.text += Dialogue.yourName[j];
+                    yield return new WaitForSeconds(0.01f);
+                }  
+            }
+            else if (listSentences[count][i].Equals('뀽')) // 상대방 이름일 때
+            {
+                for (int j = 0; j < Dialogue.herName.Length; j++)
+                {
+                    text.text += Dialogue.herName[j];
+                    yield return new WaitForSeconds(0.01f);
+                }
+            }
+            else // 이름이 아닐 때
+            {
+                text.text += listSentences[count][i]; // 1번째 문장, 가나다라마바사 한 글자씩 출력
+            }
             yield return new WaitForSeconds(0.01f); // 출력 사이에 0.01초 딜레이 줌
         }
     }
