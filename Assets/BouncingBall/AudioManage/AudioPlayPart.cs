70acf0430710579ac79fd0a097a2ef68aa61d5a7
minkpang <pmink210@gmail.com>
2021-11-12 17:46:19 +0900

    [fix] ball stage, audio

--- Assets/BouncingBall/AudioManage/AudioPlayPart.cs
+++ Assets/BouncingBall/AudioManage/AudioPlayPart.cs
@@ -22,7 +22,7 @@ public class AudioPlayPart : MonoBehaviour
         musicpart = gameObject;
         audioSource = GetComponent<AudioSource>();
 
-        if(SceneManager.GetActiveScene().name == "Stage")
+        if(SceneManager.GetActiveScene().name == "DishGameStartScene")
         {
             Destroy(musicpart);
         }
